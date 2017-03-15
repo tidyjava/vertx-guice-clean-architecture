723a2e972a9e87c5528f43d960449c077cbcaf98
Grzegorz Ziemoński <grzegorz.ziemonski@gmail.com>
2017-03-15 21:37:40 +0100

    Reformat code.

--- src/main/java/com/tidyjava/example/gateways/ActivityGateway.java
+++ src/main/java/com/tidyjava/example/gateways/ActivityGateway.java
@@ -4,7 +4,6 @@ import com.tidyjava.example.callback.Callback;
 import com.tidyjava.example.entities.Activity;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 public interface ActivityGateway {
     void findAll(Callback<List<Activity>> callback);
